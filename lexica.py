e117151c476c44bdecc1a4a6e4b8cb986391a654
Quentin Denigot <q.denigot@gmail.com>
2021-10-15 10:54:42 +0200

    added lexicon, lex and pers classes

--- lexica.py
+++ lexica.py
@@ -1,43 +1,45 @@
-# This is the lexica class. Given a lexicon name it will basically generate
-# a table containing the truth-values for a given utterance.
+# This is the Lexicon class. It serves as a basis for semantic and social
+# lexica.
+class Lexicon:
+    def __init__(self, utt_dic) -> None:
+        self._utt_dic = utt_dic
+        self.lexical_units = [k for k in utt_dic.keys()]
 
-class Lex:
-    def __init__(self, utterances) -> None:
-        self.utterances = utterances
-        self.__lex_construction()
+# This is the semantics lexicon
+class Lex(Lexicon):
+    def __init__(self, utt_dic) -> None:
+        super().__init__(utt_dic)
+        self.__build_semantics()
 
-    def __lex_construction(self):
+    def __build_semantics(self):
+        self.__semantics = {}
+        for k in self._utt_dic.keys():
+            self.__semantics[k] = self._utt_dic[k]["worlds"]
+        return self.__semantics
 
-        def __industrial():
-            self.semantics = ["competent", "incompetent"]
-            self.interpretation_function = {"ing": [self.properties[0]],
-                                            "in": [self.properties[1]]}
-        def __civic():
-            self.properties = ["friendly", "unfriendly"]
-            self.interpretation_function = {"ing": [self.properties[1]],
-                                            "in": [self.properties[0]]}
-        def __inspired():
-            self.properties = ["insightful", "uninsightful"]
-            self.interpretation_function = {"ing": [self.properties[0], self.properties[1]],
-                                            "in": [self.properties[0], self.properties[1]]}
-        def __domestic():
-            self.properties = ["traditional", "non-traditional"]
-            self.interpretation_function = {"ing": [self.properties[0]],
-                                            "in": [self.properties[1]]}
-        def __fame():
-            self.properties = ["cool", "uncool"]
-            self.interpretation_function = {"ing": [self.properties[1]],
-                                                "in": [self.properties[0]]}
+    def semantics(self, lexical_unit):
+        if lexical_unit in self.lexical_units:
+            return self.__semantics[lexical_unit]
+        else:
+            print("This lexical unit is not defined in the lexicon")
 
-        wc = {
-            "industrial": __industrial,
-            "civic": __civic,
-            "inspired": __inspired,
-            "domestic": __domestic,
-            "fame": __fame,
-        }
+# This is the social meaning lexicon
+class Pers(Lexicon):
+    def __init__(self, utt_dic) -> None:
+        super().__init__(utt_dic)
+        self.__build_social_meaning(self)
 
-        return wc.get(self.lex_name, "Not a valid world name.")()
+    def __build_social_meaning(self):
+        self.social_meaning = {}
+        for k in self._utt_dic.keys():
+            self.__social_meaning[k] = self._utt_dic[k]["personae"]
+        return self.__social_meaning
+
+    def social_meaning(self, lexical_unit):
+        if lexical_unit in self.lexical_units:
+            return self.__social_meaning[lexical_unit]
+        else:
+            print("This lexical unit is not defined in the social meaning lexicon")
 
 # This class is the priors, it is a special kind of dictionary to make it
 # easy to store the priors for each player in the correct format.
